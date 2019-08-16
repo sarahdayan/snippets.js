65f0eb614072fd451a33a4baf87c48b865455019
Sarah Dayan <dayan.sarah@gmail.com>
2019-08-16 15:11:20 +0200

    fix: remove unused C# sub-project

--- packages/example/snippets/csharp/snippets/getSettings.cs
+++
@@ -1,9 +0,0 @@
-IndexSettings settings = new IndexSettings
-{
-  CustomRanking = new List<string> { "desc(followers)" }
-};
-
-index.SetSettings(setting);
-
-// Asynchronous
-await index.SetSettingsAsync(settings);
