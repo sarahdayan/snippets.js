38d6cc078095d08e77ca593006c50f2fe742e6da
Sarah Dayan <sarah.dayan@algolia.com>
2019-08-20 11:36:21 +0200

    build: ignore in command to avoid breaking test command

--- packages/snippets.js/babel.config.js
+++ packages/snippets.js/babel.config.js
@@ -11,11 +11,5 @@ module.exports = {
     ],
     '@babel/preset-typescript'
   ],
-  plugins: ['add-module-exports'],
-  ignore: [
-    '**/interfaces/*',
-    '**/__tests__/*',
-    '**/__helpers__/*',
-    '**/unindent.d.ts'
-  ]
+  plugins: ['add-module-exports']
 }
